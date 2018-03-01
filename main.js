38e089ee9fa501fe2a759f2a68624dbe0f433f89
Piotr Ptak <robakzdobywca@gmail.com>
2018-03-01 19:53:59 +0100

    parameters input

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const configParser = require('./utils/configParser');
 const pathCost = require('./utils/pathCost');
+const sets = [
+  './sets/a_example.in',
+  './sets/b_should_be_easy.in',
+  './sets/c_no_hurry.in',
+  './sets/d_metropolis.in',
+  './sets/e_high_bonus.in'
+]
 
 class SelfDrivingRidesAnalizer {
   constructor(filename) {
@@ -31,6 +38,27 @@ class SelfDrivingRidesAnalizer {
   }
 }
 
-const analizer = new SelfDrivingRidesAnalizer('./sets/a_example.in');
+const analizers = [];
 
-console.log(JSON.stringify(analizer, null, 2));
+if(process.argv.length == 2) {
+  //if there is no console parameter, run for every input set
+  sets.forEach(set => {
+    analizers.push(new SelfDrivingRidesAnalizer(set));
+  });
+
+} else {
+  for(let i=2; i<process.argv.length; i++) {
+    switch(process.argv[i]) {
+      case 'a': analizers.push(new SelfDrivingRidesAnalizer(sets[0])); break;
+      case 'b': analizers.push(new SelfDrivingRidesAnalizer(sets[1])); break;
+      case 'c': analizers.push(new SelfDrivingRidesAnalizer(sets[2])); break;
+      case 'd': analizers.push(new SelfDrivingRidesAnalizer(sets[3])); break;
+      case 'e': analizers.push(new SelfDrivingRidesAnalizer(sets[4])); break;
+    }
+  }
+}
+
+//TODO ALL THE CALCULATIONS!
+analizers.forEach(analizer => {
+  console.log(JSON.stringify(analizer, null, 2));
+});
